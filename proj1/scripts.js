e8e920f8af8e59ce31c40624efd118e174da0850
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-01-31 16:34:47 +0500

    Update scripts.js

--- proj1/scripts.js
+++ proj1/scripts.js
@@ -1,1 +1,16 @@
-console.log("hey fromm intel side") 
+console.log("sum is: ") 
+var a=6
+var b=9
+console.log(a+b)
+console.log(typeof a)
+
+
+
+let c={
+    "name":"charlie",
+    "age":34,
+    "job":null
+}
+
+console.log(c)
+console.log(typeof c.job)
