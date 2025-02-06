61d82180d59871e0ec88cd4be124a04c1b93c84f
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-02-07 01:36:16 +0500

    Update scripts.js

--- proj1/scripts.js
+++ proj1/scripts.js
@@ -1,59 +1,15 @@
 
-let a = parseInt( prompt("enter first number"))
-let b = parseInt(prompt("enter second number")) 
-
-let opt = prompt("1 for addition\n2 for substraction\n3 for mutiplication\n4 for division")
-
-let r = Math.random() * 10
 
 
 
+var nme="ghayur"
 
-// if(r<10){
-//     switch (opt) {
-//         case "1":
-//             alert("ans is"+a / b)
-//             break;
-    
-//         case "2":
-//             alert("ans is"+a ** b)
-//             break;
-    
-//         case "3":
-//             alert("ans is"+(a + b))
-//             break;
+console.log(`my nam eis ${nme}`)
 
-    
-//         case "4":
-//             alert("ans is"+a - b)
-//             break;
-//         default:
-//             alert("wrong ans lol")
-//             break;
-//     }
-// }
+console.log(nme.replace("a",'d'))
 
-// else{
-    switch (opt) {
-        case "1":
-            alert(a + b)
-            break;
-    
-        case "2":
-            alert(a - b)
-            break;
-    
-        case "3":
-            alert(a * b)
-            break;
-    
-        case "4":
-            alert(a / b)
-            break;
-        default:
-            alert("corrected calculate sanser")
-            break;
-    }
-// }
+let a=9;
+let b=8;
 
-
+console.log(`answer is: ${eval(`${a}+${b}`)}`)
+console.log(a+'+'+b)
