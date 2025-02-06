f5c0e9c6f4b5c36073c61c51c01d6b897d064bf6
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-02-06 20:17:00 +0500

    Update scripts.js

--- proj1/scripts.js
+++ proj1/scripts.js
@@ -1,16 +1,1 @@
-console.log("sum is: ") 
-var a=6
-var b=9
-console.log(a+b)
-console.log(typeof a)
-
-
-
-let c={
-    "name":"charlie",
-    "age":34,
-    "job":null
-}
-
-console.log(c)
-console.log(typeof c.job)
+console.log("hello javascripts")
