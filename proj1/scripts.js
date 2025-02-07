d45eeb2cd7b7ab373f885664b89f522695eeef4c
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-02-07 19:59:51 +0500

    Update scripts.js

--- proj1/scripts.js
+++ proj1/scripts.js
@@ -1,27 +1,25 @@
+var a = {
+    1: "sparky",
+    2: "cool",
+    3: "funky"
+};
 
+var b = {
+    1: "shoes",
+    2: "clothes",
+    3: "cars"
+};
 
+var c = {
+    1: "limited",
+    2: "copyeright",
+    3: "group"
+};
 
+// Generate a random number between 1 and 3
+var r = Math.floor(Math.random() * 4);
 
+// Construct the name
+var output = a[r] + " " + b[r] + " " + c[r];
 
-var a={
-    1:"sparky",
-    2:"cool",
-    3:"funky"
-}
-
-var b={
-    1:"shoes",
-    2:"clothes",
-    3:"cars"
-}
-
-var c={
-    1:"limited",
-    2:"copyeright",
-    3:"group"
-}
-
-
-var r=Math.random
-
-while(r>10)
+console.log(output);
