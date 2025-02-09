29a0330122de63b6867e4a99bb6a9c97adc83f8f
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-02-09 20:05:16 +0500

    u

--- proj1/scripts.js
+++ proj1/scripts.js
@@ -1,9 +1,1 @@
-function fac(num) {
-    if (num === 0 || num === 1) {
-        return 1;  // Base case: factorial of 0 or 1 is 1
-    }
-    return num * fac(num - 1);  // Recursive call
-}
-
-var num = 5;
-console.log(fac(num)); // Output: 120
+let a=document.getElementsByClassName("nav")
