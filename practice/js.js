79bfbcab5d66d498ebab35994918020fd576ffc6
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-07-19 15:48:39 +0500

    Update js.js

--- practice/js.js
+++ practice/js.js
@@ -1,1 +1,25 @@
 //faulty calculator
+
+let num1=7
+let num2=2
+
+
+let r=Math.random()*10
+r=Math.floor(r)
+console.log(r)
+
+
+if(r<=2){
+    console.log("wrong👎")
+console.log("Addition resut is: ",num1-num2)
+console.log("substraction resut is: ",num1+num2)
+console.log("Multiplication resut is: ",num1/num2)
+console.log("Division resut is: ",num1+num2)
+}
+else{
+     console.log("correct💪")
+console.log("Addition resut is: ",num1+num2)
+console.log("substraction resut is: ",num1-num2)
+console.log("Multiplication resut is: ",num1*num2)
+console.log("Division resut is: ",num1/num2)
+}
