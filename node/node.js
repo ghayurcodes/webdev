1e21e29ccaaa89aa587dae2b887711002fa1dde9
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-07-28 23:13:40 +0500

    u

--- node/node.js
+++ node/node.js
@@ -1,13 +1,14 @@
-const fs=require("fs")
+// Step 1: Import express
+const express = require('express');
+const app = express();
+const port = 3000;
 
-console.log("starting")
-fs.readFile("tetx.txt",(e,d)=>{
-   
-    if(e){
-        console.log(e)
-    }
-    else{
-        console.log(d.toString())
-    }
-})
-console.log("ending")
+// Step 2: Define a route
+app.get('/', (req, res) => {
+  res.send('Hello from Express! hello ghayur king of the pakistan');
+});
+
+// Step 3: Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
