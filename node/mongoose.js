19c92537345124a483af156a391d2d225f220b17
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-07-30 12:28:02 +0500

    u

---
+++ node/mongoose.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+try {
+  await mongoose.connect("mongodb://localhost:27017/test");
+  console.log("MongoDB connected");
+} catch (err) {
+  console.error("Connection error:", err);
+}
+
+
+
+
+
+// Define a schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+// Create a model
+const User = mongoose.model("users", userSchema);
+
+// const user=new User({
+//     "name":"ali",
+//     "age":99
+// })
+
+// user.save()
+
+
+console.log( await User.find())
+
