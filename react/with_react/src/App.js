1f0d17261aaf3dd55a9e6bca6af8b60de158cbe1
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-08-05 11:44:46 +0500

    u

--- react/with_react/src/App.js
+++ react/with_react/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>hello kalia {value}</p>
+      <button onClick={() => setValue(value+1)}>tap me</button>
     </div>
   );
 }
