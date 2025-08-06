1e3f33a98b18afd9fe8b0279dc27ccc525bc17f4
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-08-06 16:24:00 +0500

    Update App.js

--- react/with_react/src/App.js
+++ react/with_react/src/App.js
@@ -3,15 +3,22 @@ import React, { useState ,useEffect, useRef} from 'react';
 import Navbar from './component/nav'; // Make sure the filename is nav.js and the folder path is correct
 
 function App(props) {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);      //usestate(hooks)
   let a=useRef(0)
+  let [showbtn, setshowbtn] = useState(false);
   
 
-  useEffect(() => {
-    a.current=a.current+1
-  alert("hello" +a.current)
+  // useEffect(() => {              //useeffect
+  //   a.current=a.current+1     //setrefuse
+  // alert("hello" +a.current)
  
-  }, [value])
+  // }, [value])
+
+  useEffect(() => {
+   if(value==10){
+    setshowbtn(true)              //conditional rendering
+   }
+  }, )
 
  
   return (
@@ -19,6 +26,7 @@ function App(props) {
       <Navbar /> 
       <p>Hello {props.name}</p>
       <p>{value}</p>
+      {showbtn &&<p>yay button hits 10</p>}
       <button className='btn' onClick={() => setValue(value + 1)}>Tap me</button>
       
     </div>
