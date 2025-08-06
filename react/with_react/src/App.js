00155f01e75fdbb0cc7653bbd1f43738548b029c
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-08-06 12:34:04 +0500

    update

--- react/with_react/src/App.js
+++ react/with_react/src/App.js
@@ -1,14 +1,16 @@
-
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import Navbar from './component/nav'; // Make sure the filename is nav.js and the folder path is correct
 
-function App() {
+function App(props) {
   const [value, setValue] = useState(0);
 
   return (
     <div className="App">
-      <p>hello kalia {value}</p>
-      <button onClick={() => setValue(value+1)}>tap me</button>
+      <Navbar /> {/* Capitalized usage */}
+      <p>hello kalia {props.name}</p>
+      <p>{value}</p>
+      <button onClick={() => setValue(value + 1)}>tap me</button>
     </div>
   );
 }
---
+++ react/with_react/src/component/nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './nav.css'; // ✅ Correct way to import CSS in React
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>support</li>
+        <li>buy</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
