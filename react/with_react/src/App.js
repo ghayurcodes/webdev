49784e0e076fead61ed1176e72c29f7623854bf7
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-08-06 16:05:23 +0500

    update

--- react/with_react/src/App.js
+++ react/with_react/src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState ,useEffect, useRef} from 'react';
 import Navbar from './component/nav'; // Make sure the filename is nav.js and the folder path is correct
 
 function App(props) {
   const [value, setValue] = useState(0);
+  let a=useRef(0)
+  
 
+  useEffect(() => {
+    a.current=a.current+1
+  alert("hello" +a.current)
+ 
+  }, [value])
+
+ 
   return (
     <div className="App">
-      <Navbar /> {/* Capitalized usage */}
-      <p>hello kalia {props.name}</p>
+      <Navbar /> 
+      <p>Hello {props.name}</p>
       <p>{value}</p>
       <button className='btn' onClick={() => setValue(value + 1)}>Tap me</button>
+      
     </div>
   );
+  
 }
 
 export default App;
--- react/with_react/src/index.js
+++ react/with_react/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <App  name="ahmed"/>
+    <App  name="Ghayur"/>
  
 );
 
